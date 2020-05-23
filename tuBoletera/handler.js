140908b91300b71943c7ead6f4c3b2ad3b64f082
Manu Barral <manuelbarral02@gmail.com>
2020-05-23 16:42:05 -0300

    Change handler - api methods

--- tuBoletera/handler.js
+++ tuBoletera/handler.js
@@ -3,19 +3,32 @@
 const AWS = require('aws-sdk');
 let dynamo = new AWS.DynamoDB.DocumentClient();
 
-const TABLE_NAME = 'ExampleTable';
+const TABLE_NAME = 'TuBoleteraTable';
+const TEST_PAGE_VISIT = 'TestPageVisit';
+const TEST_KNOW_MORE = 'TestKnowMore';
+const PAGE_VISIT = 'PageVisit';
+const KNOW_MORE = 'KnowMore';
 
-exports.saveHello = async (event) => {
-  console.log('hola');
-  const name = event.queryStringParameters.name;
-  
-  const item = {
-    name: name,
-    date: Date.now()
+addToItem = (item) => {
+  if (item.value) {
+    return {
+      name: item.name,
+      value: item.value + 1
+    }
+  } else {
+    return {
+      name: item.name,
+      value: 1
+    }
   }
-  
-  console.log(item);
-  const savedItem = await saveItem(item);
+}
+
+exports.testPageVisit = async (event) => {
+  console.log('test page visit');
+
+  const item = await getItem(TEST_PAGE_VISIT);
+  const newItem = addToItem(item);
+  const savedItem = await saveItem(newItem);
 
   return {
     statusCode: 200,
@@ -26,22 +39,71 @@ exports.saveHello = async (event) => {
   }
 }
 
-exports.getHello = async (event) => {
-  const name = event.queryStringParameters.name;
-  
-  try {
-    const item = await getItem(name);
-    console.log(item);
+exports.testKnowMore = async (event) => {
+  console.log('test know more');
 
-    if (item.date) {
-      const d = new Date(item.date);
-      
+  const item = await getItem(TEST_KNOW_MORE);
+  const newItem = addToItem(item);
+  const savedItem = await saveItem(newItem);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(savedItem),
+  }
+}
+
+exports.pageVisit = async (event) => {
+  console.log('page visit');
+
+  const item = await getItem(PAGE_VISIT);
+  const newItem = addToItem(item);
+  const savedItem = await saveItem(newItem);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(savedItem),
+  }
+}
+
+exports.knowMore = async (event) => {
+  console.log('know more');
+
+  const item = await getItem(KNOW_MORE);
+  const newItem = addToItem(item);
+  const savedItem = await saveItem(newItem);
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(savedItem),
+  }
+}
+
+exports.getTestData = async (event) => {
+  console.log('get test data');
+
+  try {
+    const tpvItem = await getItem(TEST_PAGE_VISIT);
+    const tkmItem = await getItem(TEST_KNOW_MORE);
+
+    if (tpvItem.value && tkmItem.value) {
       return {
         statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*'
         },
-        body: `Was greeted on ${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`
+        body: {
+          testPageVisit: tpvItem.value,
+          testKnowMore: tkmItem.value
+        }
       }
     }
   } catch (e) {
@@ -51,39 +113,76 @@ exports.getHello = async (event) => {
       headers: {
         'Access-Control-Allow-Origin': '*'
       },
-      body: 'Nobody was greeted with that name'
-  }
+      body: {
+        testPageVisit: -1,
+        testKnowMore: -1
+      }
+    }
   }
 }
 
- async function getItem (name) {
-    console.log('getItem');
-    
-    const params = {
-      Key: {
-        name: name,
-      },
-      TableName: TABLE_NAME
-    };
+exports.getData = async (event) => {
+  console.log('get data');
 
-    console.log(params);
-  
-    return dynamo.get(params).promise().then(result => {
-        console.log(result);
-        return result.Item;
-    });
+  try {
+    const pvItem = await getItem(PAGE_VISIT);
+    const kmItem = await getItem(KNOW_MORE);
+
+    if (pvItem.value && kmItem.value) {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: {
+          pageVisit: pvItem.value,
+          knowMore: kmItem.value
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: {
+        pageVisit: -1,
+        knowMore: -1
+      }
+    }
+  }
+}
+
+async function getItem(name) {
+  console.log('getItem');
+
+  const params = {
+    Key: {
+      name: name,
+    },
+    TableName: TABLE_NAME
+  };
+
+  console.log(params);
+
+  return dynamo.get(params).promise().then(result => {
+    console.log(result);
+    return result.Item;
+  });
 };
 
 
- async function saveItem(item) {
-	const params = {
-		TableName: TABLE_NAME,
-		Item: item
-	};
+async function saveItem(item) {
+  const params = {
+    TableName: TABLE_NAME,
+    Item: item
+  };
 
   console.log(params)
 
-	return dynamo.put(params).promise().then(() => {
+  return dynamo.put(params).promise().then(() => {
     return item;
   });
 };
